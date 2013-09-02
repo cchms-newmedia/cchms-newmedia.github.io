d0fdf87909b6c6b905f12645610276800873d230
ttwj <terencetanwj@gmail.com>
2013-09-02 22:07:28 +0800

    scrollspy

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var offset = 200;
+
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
     
 	// Cache the Window object
 	$window = $(window);
